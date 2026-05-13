1e9d64335f410f56b26c800bb60734e6620eaae2
HakkinDavid <desr11@hotmail.com>
2026-05-12 22:52:11 -0700

    chatty peak tea

---
+++ 12May.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+SYSTEM_MESSAGE = "You are a chatbot. You will have a conversation with a user. Be friendly and concise"
+
+if __name__ == "__main__":
+    load_dotenv()
+    URL = os.environ.get('OPENAI_BASE_URL')
+    KEY = os.environ.get('OPENAI_KEY')
+    MODEL = os.environ.get('MODEL')
+
+    client = OpenAI(
+        base_url=URL,
+        api_key=KEY,
+    )
+
+    print(f"Chatting with {MODEL} model at {URL}\n")
+    
+    messages = [{'role': 'system', 'content': SYSTEM_MESSAGE}]
+
+    while True:
+        message = input("> ")
+        messages.append({'role': 'user', 'content': message})
+        response = client.chat.completions.create(
+            model=MODEL,
+            n=1,
+            messages=messages
+        )
+        print(response.choices[0].message.content)
+        messages.append(response.choices[0].message)
